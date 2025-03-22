3269d74b3608c5fb65c5d8d5f4fe13736a68c8c0
Zaid Zaidan <zaidzaidan79@gmail.com>
2025-03-23 02:48:33 +0300

    finish footer component

---
+++ src/app/components/Footer.js
@@ -0,0 +1,46 @@
+import React from "react";
+import { Facebook, Be, Instagram, LinkedIn, Website } from "../icons";
+import Link from "next/link";
+import Image from "next/image";
+const Footer = () => {
+  const socialMediaLinks = [
+    {
+      icon: Facebook,
+      link: "test",
+    },
+    {
+      icon: Website,
+      link: "test",
+    },
+    {
+      icon: LinkedIn,
+      link: "test",
+    },
+    {
+      icon: Instagram,
+      link: "test",
+    },
+    {
+      icon: Be,
+      link: "test",
+    },
+  ];
+  return (
+    <div className="flex px-24 pt-14 pb-[87px] pr-[348px] justify-between items-center">
+      <h2 className="font-semibold text-sm">
+        Israa Elshebli - Skincare Backed by Science.
+      </h2>
+      <div className="flex gap-2">
+        {socialMediaLinks.map((socialLink, index) => {
+          return (
+            <Link key={index} href={socialLink.link} target="_blank">
+              <Image src={socialLink.icon} alt="socialMedia"></Image>
+            </Link>
+          );
+        })}
+      </div>
+    </div>
+  );
+};
+
+export default Footer;
---
+++ src/app/icons.js
@@ -0,0 +1,7 @@
+import Be from "../../public/icons/Be.svg";
+import Facebook from "../../public/icons/facebook.svg";
+import Instagram from "../../public/icons/instagram.svg";
+import LinkedIn from "../../public/icons/linkedin.svg";
+import Website from "../../public/icons/website.svg";
+
+export { Be, Facebook, Instagram, LinkedIn, Website };
